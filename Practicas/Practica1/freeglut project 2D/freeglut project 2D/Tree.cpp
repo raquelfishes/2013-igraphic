3d8bcdb309f1c3ff82ec33b70dd2b0a4af42f88f
Raquelpm23@gmail.com <Raquelpm23@gmail.com@2188cf36-fe29-5e81-ddad-f2da4cde5937>
2013-11-02 16:26:02 +0000

    cambio metodo seleccion

--- Practicas/Practica1/freeglut project 2D/freeglut project 2D/Tree.cpp
+++ Practicas/Practica1/freeglut project 2D/freeglut project 2D/Tree.cpp
@@ -234,7 +234,18 @@ void Tree::searchSquare(GLdouble x, GLdouble y){
 	for (unsigned i=0; i<estructura->size(); i++){
 		aux_level = estructura->at(i);
 		for (unsigned j=0; j<aux_level->size(); j++){
-			if(aux_sq==NULL) {
+			aux_sq = aux_level->at(j);
+			cout<< "probando cuadrado " << i <<" "<< j<< endl;
+			aux_dist = point->distance(aux_sq->getClosestCorner(point));
+			cout<< "distancia al cuadrado " << i <<" "<< j<< "= "<< aux_dist << endl;
+			if (aux_dist<20){
+				cout<< "me gusta el cuadrado " << i <<" "<< j<< endl;
+				aux_sq->setSelec(true);
+			}
+			else{
+				aux_sq->setSelec(false);
+			}
+			/*if(aux_sq==NULL) {
 				aux_sq = aux_level->at(j);
 				aux_dist = point->distance(aux_sq->getClosestCorner(point));
 			}
@@ -247,7 +258,7 @@ void Tree::searchSquare(GLdouble x, GLdouble y){
 					aux_sq->setSelec(true);
 				}
 				aux_point = NULL;
-			}
+			}*/
 		}
 	}
 
