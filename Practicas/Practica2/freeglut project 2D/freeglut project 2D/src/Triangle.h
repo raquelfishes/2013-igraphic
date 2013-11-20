ae4d6e2ea5d35b3a278390606f35c2f5d8d59e09
Raquelpm23@gmail.com <Raquelpm23@gmail.com@2188cf36-fe29-5e81-ddad-f2da4cde5937>
2013-11-20 11:44:21 +0000

    blabla

--- Practicas/Practica2/freeglut project 2D/freeglut project 2D/src/Triangle.h
+++ Practicas/Practica2/freeglut project 2D/freeglut project 2D/src/Triangle.h
@@ -1,22 +1,26 @@
 #pragma once
 
 #include "Obstacle.h"
-class Triangle : public Obstacle
+#include "DrawablePolygon.h"
+
+class Triangle : public Obstacle, public DrawablePolygon
 {
 private:
 	std::vector<PV2D*> *vertex;
 	std::vector<PV2D*> *normal;
 	Pencil* pencil;
 	PV2D* center;
+	GLdouble radius;
 
 public:
 	Triangle(void);
 	~Triangle(void);
 	Triangle(vector<PV2D*> *v);
 	Triangle(PV2D* p1, PV2D* p2, PV2D* p3);
+	Triangle(PV2D* p, GLdouble r);
 	
-	void render(void){ cout << "hago un render de mi triangulo" << endl;}
-	void collide(Ball *ball) {}
+	void render(void);
+	void collide(Ball *ball);
 	void doNormals(void);
 	void doVertex(void);
 };
