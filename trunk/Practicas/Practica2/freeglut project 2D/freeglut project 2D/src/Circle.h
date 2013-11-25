4f35e50585e37f5ab2a0d89ea88ac25d68e76d3a
Raquelpm23@gmail.com <Raquelpm23@gmail.com@2188cf36-fe29-5e81-ddad-f2da4cde5937>
2013-11-25 20:12:13 +0000

    creando el circulo y empezando a rebotar

--- trunk/Practicas/Practica2/freeglut project 2D/freeglut project 2D/src/Circle.h
+++ trunk/Practicas/Practica2/freeglut project 2D/freeglut project 2D/src/Circle.h
@@ -11,76 +11,45 @@ public:
 	Circle(PV2D *c,GLdouble r);
 	~Circle(void);
 	bool collide(Ball *ball, GLdouble& tin, PV2D*& normalIn){
-		bool retValue = false;
-		retValue = this->getCenter()->distance(ball->getCenter()) < ball->getRadius()+this->getRadius();
-		if(retValue){
-		// TODO calcular la normal
+		GLdouble A = ball->getVector()->scalarProduct(ball->getVector());
+		PV2D* aux_v = ball->getCenter()->substract(getCenter());
+		GLdouble B = (aux_v->scalarProduct(ball->getVector())*2);
+		GLdouble C = (aux_v->scalarProduct(aux_v))-(pow(this->getRadius(),2));
+		GLdouble dis = (pow(B,2)) - (4*A*C);
+		delete aux_v;
+
+		if (dis<0){
+			return false;
 		}
-		return retValue;
+		if (dis == 0){
+			//Hay una intersección
+			tin = (-B)/(2*A);
+			PV2D* aux_v=ball->getVector()->factor(tin);
+			normalIn=ball->getCenter()->substract(aux_v);
+            delete aux_v;
+            return true;
+		}
+		if (dis>0){
+			GLdouble t1 = ((-B)-sqrt(dis))/(2*A);
+			GLdouble t2 = ((-B)+sqrt(dis))/(2*A);
 
+			if(t1>0 && t2>0)
+				{ tin=min(t1,t2); }
+			if(t1>0 && t2<0)
+				{ tin=t1; }
+            if(t1<0 && t2>0)
+				{ tin=t2; }
+			if(t1<0 && t2<0)
+				{ return false; }
 
-		
-		/*
-		 GLdouble a=vel->dot(vel);
-        PV2D* cp=pelota->resta(centro);
-        GLdouble b=cp->dot(vel)*2;
-        GLdouble c=(cp->dot(cp))-pow(radio,2);
+			PV2D* aux_v=ball->getVector()->factor(tin);
+            PV2D* aux_b=ball->getCenter()->add(aux_v);
 
-        GLdouble discriminante=  pow(b,2)-4*(a*c);
-        delete cp;
-
-        if(discriminante<0)
-        {
-         return false;
-        }
-
-        if(discriminante==0)
-        {
-              tIn=(-b)/(2*a);
-
-              PV2D* velaux=vel->factor(tIn);
-              normalIn=centro->resta(velaux); //TODO ESTO ESTA MAL
-              delete velaux;
-              return true;
-
-        }
-
-        if(discriminante>0)
-        {
-              GLdouble t1=((-b)-sqrt(discriminante))/(2*a);
-              GLdouble t2=((-b)+sqrt(discriminante))/(2*a);
-
-              if(t1>0 && t2>0)
-              {
-               tIn=min(t1,t2);
-              }
-              if(t1<0 && t2<0)
-              {
-                  return false;
-              }
-
-              if(t1<0 && t2>0)
-              {
-               tIn=t2;
-              }
-
-              if(t1>0 && t2<0)
-              {
-               tIn=t1;
-              }
-
-              PV2D* paux=vel->factor(tIn);
-              PV2D* pelaux=pelota->suma(paux);
-
-              normalIn=centro->resta(pelaux);
-              delete paux;
-              delete pelaux;
-              return true;
-        }
-
-
-         return false;
-		*/
+			normalIn=ball->getCenter()->substract(aux_b);
+			delete aux_v;
+            delete aux_b;
+            return true;
+		}
 	};
 
 };
