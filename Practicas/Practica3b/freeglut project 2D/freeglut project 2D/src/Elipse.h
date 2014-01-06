173916308b70c24c875ae18b4b7e801fb492e2a1
cacu2ggg <cacu2ggg@2188cf36-fe29-5e81-ddad-f2da4cde5937>
2014-01-06 19:54:57 +0000

    a puntito de acabar la elipse

--- Practicas/Practica3b/freeglut project 2D/freeglut project 2D/src/Elipse.h
+++ Practicas/Practica3b/freeglut project 2D/freeglut project 2D/src/Elipse.h
@@ -11,9 +11,9 @@
 class Elipse :	public Obstacle
 {
 private:
-	GLfloat matrix[16];
-	GLfloat inv_matrix[16];
-
+	//GLfloat matrix[16];
+	//GLfloat inv_matrix[16];
+	GLdouble propX,propY;
 
 public:
 	Elipse(void);
@@ -21,7 +21,7 @@ public:
 	~Elipse(void);
 	PV2D* multMatriz(GLfloat* matrix, PV2D* punt, int tipo);
 	PV2D* multMatrizT(GLfloat* matrix, PV2D* punt, int tipo);
-	bool collide(Ball *ball, GLdouble& tin, PV2D*& normalIn){
+	/*bool collide(Ball *ball, GLdouble& tin, PV2D*& normalIn){
 
 		// De alguna forma hay que recoger la matriz que se le aplica a la elipse y aplicarsela al centro y la velocidad de la pelota
 		//Aplicar M a la pelota (centro y vector)
@@ -106,14 +106,96 @@ public:
 
 
          return false;
+	}*/
+
+	bool collide(Ball *ball, GLdouble& tin, PV2D*& normalIn){
+
+		PV2D *bVectorTransformed;	// ball->getVector()
+		PV2D *bCenterTransformed;	// ball->getCenter()
+
+		// Transformamos la información que lleva la pelota para hacer bien la colisión
+
+
+
+		GLdouble a = bVectorTransformed->scalarProduct(bVectorTransformed);
+		PV2D* cp = bCenterTransformed->substract(new PV2D());
+		GLdouble b=cp->scalarProduct(bVectorTransformed)*2;
+		GLdouble c=(cp->scalarProduct(cp))-pow(1,2);
+
+        GLdouble discriminante=pow(b,2)-4*(a*c);
+        delete cp;
+
+		cout <<"discriminante="<<discriminante<<endl;
+
+        if(discriminante<0)
+        {
+		 cout <<"discriminante<0"<<endl;
+         return false;
+        }
+
+        if(discriminante==0)
+        {
+			  cout <<"discriminante=0"<<endl;
+              tin=(-b)/(2*a);
+
+			  PV2D* velaux = bVectorTransformed->factor(tin);
+			  normalIn = bCenterTransformed->substract(velaux); //TODO ESTO ESTA MAL
+              delete velaux;
+              return true;
+
+          }
+
+        if(discriminante>0)
+        {
+			  cout <<"discriminante>0"<<endl;
+              GLdouble t1=((-b)-sqrt(discriminante))/(2*a);
+              GLdouble t2=((-b)+sqrt(discriminante))/(2*a);
+
+              if(t1>0 && t2>0)
+              {
+               tin=min(t1,t2);
+              }
+              if(t1<0 && t2<0)
+              {
+                  return false;
+              }
+
+              if(t1<0 && t2>0)
+              {
+               tin=t2;
+              }
+
+              if(t1>0 && t2<0)
+              {
+               tin=t1;
+              }
+
+              PV2D* paux = bVectorTransformed->factor(tin);
+              PV2D* pelaux = bCenterTransformed->add(paux);
+
+			  normalIn= new PV2D()->substract(pelaux);
+              delete paux;
+              delete pelaux;
+              return true;
+        }
+
+
+		// Aplicamos las transformaciones pertinentes a tIn
+
+         return false;
+
+
 	}
+
 
 	void render(void){
 		
 		glPushMatrix();
 		//Aplicamos las transformaciones parar las elipses
 		//glLoadMatrixf(matriz);
-		glMultMatrixf(matrix);
+		//glMultMatrixf(matrix);
+		glTranslatef(getCenter()->getX(),getCenter()->getY(),0);
+		glScalef(200,100,0);
 		renderX();
 		glPopMatrix();
 		//renderX();
