ead2023404fd4a32f00a97a1de4caa161db6d70a
cacu2ggg <cacu2ggg@2188cf36-fe29-5e81-ddad-f2da4cde5937>
2014-02-23 20:57:06 +0000

    pequeños cambios para cargar la imagen q siguen fallando

--- Practicas/Practica4/freeglut project 2D/freeglut project 2D/Pixmap.cpp
+++ Practicas/Practica4/freeglut project 2D/freeglut project 2D/Pixmap.cpp
@@ -31,9 +31,9 @@ void Pixmap::fillRGBMap(unsigned char *data, int dp){
 	int i,j;
 	for (i=0; i < nRows; i++){
 		for (j=0; j < nCols; j++) {
-			rgbMap[i*nCols+j][0]=data[dp+(i*nCols+j)*3+2]; //R
-			rgbMap[i*nCols+j][1]=data[dp+(i*nCols+j)*3+1]; //G
-			rgbMap[i*nCols+j][2]=data[dp+(i*nCols+j)*3+0]; //B
+			rgbMap[i*nCols+j][0]=data[(i*nCols+j)*3]; //R
+			rgbMap[i*nCols+j][1]=data[(i*nCols+j)*3]; //G
+			rgbMap[i*nCols+j][2]=data[(i*nCols+j)*3]; //B
 		}	
 	}
 }
@@ -55,6 +55,7 @@ bool Pixmap::loadFromBufer(int width, int height, GLfloat x, GLfloat y){
 }
 
 bool Pixmap::drawMatrix(GLfloat x, GLfloat y){  // tal cual de las traspas
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glPixelStorei(GL_UNPACK_ALIGNMENT, //como escribir los pixeles 
 		1); //sin padding entre filas
 	glRasterPos2i(x, y); //esquina inferior-izquerda donde escribiremos el bloque, usando coordenadas OpenGL de la ventana
@@ -71,42 +72,61 @@ void Pixmap::rotate(GLdouble angle){
         //int puntoOrigenX = nCols / 2;
         //int puntoOrigenY = nRows / 2;
 
-        int puntoOrigenX =10;
-        int puntoOrigenY =10;
+        GLdouble puntoOrigenX = 50;
+        GLdouble puntoOrigenY = 50;
 
+        //int puntoOrigenX = 10 + nCols / 2;
+        //int puntoOrigenY = (10 + nRows / 2) * -1;
+
+        GLdouble auxX, auxY;
         int count;
         unsigned int i, j;
         for (i=1; i < nRows; i++)
                 for (j=1; j < nCols; j++) {
                         count=i*nCols + j;
+
+                        auxX = i-puntoOrigenX;
+                        auxY = j-puntoOrigenY;
+
                         //Hallamos la distancia desde el punto actual al ogigen
-                        GLdouble dist = sqrt (pow (i - puntoOrigenX, 2) +  pow(j - puntoOrigenY, 2) );
+                        GLdouble dist = sqrt (pow (auxX, 2) +  pow(auxY, 2) );
+
                         //Hallamos el angulo nuevo
-                        GLdouble auxX = i-puntoOrigenX;
-                        GLdouble auxY = j-puntoOrigenY;
-                        if(auxX =! 0 && auxY != 0){
-                        GLdouble dir = atan2(auxX, auxY);
-                        dir +=-angle;
+                        if(auxX != 0 && auxY != 0){
+                        GLdouble dir = atan2(auxY, auxX);
+                        dir +=angle;
 
-                                int puntoNuevoX = puntoOrigenX + dist * cos(dir);
-                                int puntoNuevoY = puntoOrigenY + dist * sin(dir);
+                        int puntoNuevoX = puntoOrigenX + dist * cos(dir);
+                        int puntoNuevoY = puntoOrigenY + dist * sin(dir);
 
-                                if(puntoNuevoX < nRows && puntoNuevoY < nCols ){
+                        if(puntoNuevoX < 0 || puntoNuevoX >= nRows || puntoNuevoY < 0 || puntoNuevoY >= nCols){
 
-                                        //rotacion[puntoNuevoX * nCols + puntoNuevoY][0] = pixmap[count][0];
-                                        //rotacion[puntoNuevoX * nCols + puntoNuevoY][1] = pixmap[count][1];
-                                        //rotacion[puntoNuevoX * nCols + puntoNuevoY][2] = pixmap[count][2];
-
-                                        rotacion[count][0] = rgbMap[puntoNuevoX * nCols + puntoNuevoY][0];
-                                        rotacion[count][1] = rgbMap[puntoNuevoX * nCols + puntoNuevoY][1];
-                                        rotacion[count][2] = rgbMap[puntoNuevoX * nCols + puntoNuevoY][2];
-                                }
+                             rotacion[count][0] = 255;
+                             rotacion[count][1] = 255;
+                             rotacion[count][2] = 255;
                         }
+
+
+                        if(puntoNuevoX < nRows && puntoNuevoY < nCols && puntoNuevoX >= 0 && puntoNuevoY >=0 ){
+
+                                rotacion[count][0] = rgbMap[puntoNuevoX * nCols + puntoNuevoY][0];
+                                rotacion[count][1] = rgbMap[puntoNuevoX * nCols + puntoNuevoY][1];
+                                rotacion[count][2] = rgbMap[puntoNuevoX * nCols + puntoNuevoY][2];
+
+                                //rotacion[puntoNuevoX * nCols + puntoNuevoY][0] = pixmap[count][0];
+                                //rotacion[puntoNuevoX * nCols + puntoNuevoY][1] = pixmap[count][1];
+                                //rotacion[puntoNuevoX * nCols + puntoNuevoY][2] = pixmap[count][2];
+                        }
+
+                        //rotacion[count][0] = pixmap[puntoNuevoX * nCols + puntoNuevoY][0];
+                        //rotacion[count][1] = pixmap[puntoNuevoX * nCols + puntoNuevoY][1];
+                        //rotacion[count][2] = pixmap[puntoNuevoX * nCols + puntoNuevoY][2];
+
+                }
         }
 
-		delete [] rgbMap;
+        delete [] rgbMap;
         rgbMap = rotacion;
-
 }
 
 void Pixmap::difference(Pixmap* pm){
