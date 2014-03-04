01177e9208634d6c950d798ed9dab898de59df63
cacu2ggg <cacu2ggg@2188cf36-fe29-5e81-ddad-f2da4cde5937>
2014-03-04 01:37:10 +0000

    Una rama para arreglar la rotación

---
+++ Practicas/Practica4/avecRot/freeglut project 2D/freeglut project 2D/Pencil.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Point.h"
+#include <iostream>
+using namespace std;
+
+class Pencil
+{
+private:
+	Point *pos;
+	GLdouble dir;
+
+public:
+	Pencil(void);
+	~Pencil(void);
+	Pencil(Point *p, GLdouble dir);
+	void setPos(Point *p);
+	void setDir(GLdouble d);
+	GLdouble getDir();
+	Point* getPos();
+	void turn(GLdouble a);
+	void forward(GLdouble d);
+};
+
---
+++ Practicas/Practica4/avecRot/freeglut project 2D/freeglut project 2D/Scene.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <Windows.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+#include <GL/freeglut.h>
+
+#include <iostream>
+using namespace std;
+
+#include <Tree.h>
+
+class Scene
+{
+public:
+
+	
+	// Scene visible area size
+	GLdouble xLeft, xRight, yBot, yTop;
+
+	// Scene variables
+	GLdouble xTriangle, yTriangle;
+	GLdouble triangleWidth, triangleHeight;
+
+	// Scene colors
+	GLfloat red, blue, green;
+	
+	// Tree
+	Tree* arbol;
+
+	Scene(void);
+	~Scene(void);
+	void render(void);
+	void cam_right(void);
+	void cam_left(void);
+	void cam_up(void);
+	void cam_down(void);
+	void cam_in(void);
+	void cam_out(void);
+	bool mouse_input(GLdouble x, GLdouble y);
+	bool reset(void);
+	bool tree_grow(void);
+	bool tree_drecrease(void);
+	bool read_angle(void);
+};
+
