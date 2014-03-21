d806273ed7d853b53d85d58e52d85ea3ffe4d62c
raquelpm23 <raquelpm23@2188cf36-fe29-5e81-ddad-f2da4cde5937>
2014-03-21 01:02:46 +0000

    Clase PV3D - Problemas de compilación al hacer el include de gl

--- trunk/Practicas/Practica5/freeglut project 3D/freeglut project 3D/PV3D.cpp
+++ trunk/Practicas/Practica5/freeglut project 3D/freeglut project 3D/PV3D.cpp
@@ -3,8 +3,61 @@
 
 PV3D::PV3D(void)
 {
+	this->coorX = 0;
+	this->coorY = 0;
+	this->coorZ = 0;
 }
 
+PV3D::PV3D(GLdouble x, GLdouble y, GLdouble z)
+{
+	this->coorX = x;
+	this->coorY = y;
+	this->coorZ = z;
+}
+
+GLdouble PV3D::getX()
+{
+	return coorX;
+}
+
+GLdouble PV3D::getY()
+{
+	return coorY;
+}
+
+GLdouble PV3D::getZ()
+{
+	return coorZ;
+}
+
+void PV3D::setX(GLdouble x)
+{
+	this->coorX = x;
+}
+
+void PV3D::setY(GLdouble y)
+{
+	this->coorY = y;
+}
+
+void PV3D::setZ(GLdouble z)
+{
+	this->coorZ = z;
+}
+
+PV3D* PV3D::clone(){
+	return new PV3D(this->coorX,this->coorY,this->coorZ);
+}
+
+PV3D* PV3D::addition(PV3D* p)
+{
+	return new PV3D(this->coorX+p->getX(),this->coorY+p->getY(),this->coorZ+p->getZ());
+}
+
+PV3D* PV3D::subtraction(PV3D* p)
+{
+	return new PV3D(this->coorX-p->getX(),this->coorY-p->getY(),this->coorZ-p->getZ());
+}
 
 PV3D::~PV3D(void)
 {
--- trunk/Practicas/Practica5/freeglut project 3D/freeglut project 3D/PV3D.h
+++ trunk/Practicas/Practica5/freeglut project 3D/freeglut project 3D/PV3D.h
@@ -1,12 +1,39 @@
+/***********************************
+*	Carlos Gabriel Giraldo García
+*	Raquel Peces Muñoz
+*	Práctica 4
+*
+************************************/
 #pragma once
+
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#include <GL/freeglut.h>
+
+#include <math.h>
+
 class PV3D
 {
-public:
-	double coorX;
-	double coorY;
-	double coorZ;
+private:
+	GLdouble coorX;
+	GLdouble coorY;
+	GLdouble coorZ;
 
+public:
+	
 	PV3D(void);
 	~PV3D(void);
+	PV3D(GLdouble x, GLdouble y, GLdouble z);
+	GLdouble getX();
+	void setX(GLdouble x);
+	GLdouble getY();
+	void setY(GLdouble y);
+	GLdouble getZ();
+	void setZ (GLdouble z);
+
+	PV3D* clone();
+	PV3D* addition(PV3D* v);
+	PV3D* subtraction(PV3D* v);
+	
 };
 
