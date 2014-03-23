47becdde50863aa2f85aff2ff5d0cc7efff37037
cacu2ggg <cacu2ggg@2188cf36-fe29-5e81-ddad-f2da4cde5937>
2014-03-23 21:17:13 +0000

    MontanaRusa medio añadida

--- trunk/Practicas/Practica5/freeglut project 3D/freeglut project 3D/MontanaRusa.h
+++ trunk/Practicas/Practica5/freeglut project 3D/freeglut project 3D/MontanaRusa.h
@@ -1,15 +1,48 @@
 #pragma once
 #include "Malla.h"
+//#include "Coche.h"
+#include "Polygon.h"
 
-class MontanaRusa : public Malla
-{
+class MontanaRusa : public Malla{
 private:
-	int NP;
-	int NQ;
+    GLfloat R;
+    GLfloat r;
+    GLfloat d;
+    int NP;
+    int NQ;
+    GLfloat tam;
+    GLfloat nVueltas;
+    //Coche* coche;
+    GLfloat acumCoche;
 
 public:
-	MontanaRusa(void);
+	MontanaRusa(GLfloat R,GLfloat r,GLfloat d,int NP,int NQ,GLfloat tam);
 	~MontanaRusa(void);
+
+    void construye();
+
+    GLfloat funcionX(GLfloat val);
+	GLfloat funcionY(GLfloat val);
+    GLfloat funcionZ(GLfloat val);
+
+    GLfloat primeraDerivadaX(GLfloat val);
+    GLfloat primeraDerivadaZ(GLfloat val);
+
+    GLfloat segundaDerivadaX(GLfloat val);
+    GLfloat segundaDerivadaZ(GLfloat val);
+
+    PV3D* funcion(GLfloat val);
+
+    PV3D* primeraDerivada(GLfloat val);
+    PV3D* segundaDerivada(GLfloat val);
+
+    int sucesor(int val);
+    GLfloat degToRad(GLfloat deg);
+    GLfloat calculaVueltas();
+    void dibuja(bool relleno,bool dibujaNormales);
+    void dibujaCoche();
+    void addAcum(GLfloat cantidad);
+    int mcd(int a, int b);
 
 	GLdouble functionX(GLdouble t);
 	GLdouble functionY(GLdouble t);
