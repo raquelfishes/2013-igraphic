052b2af16f68c048bbe4cdd393cecace4e0b7d2f
raquelpm23 <raquelpm23@2188cf36-fe29-5e81-ddad-f2da4cde5937>
2014-03-23 11:46:50 +0000

    bla bla

--- trunk/Practicas/Practica5/freeglut project 3D/freeglut project 3D/MontanaRusa.cpp
+++ trunk/Practicas/Practica5/freeglut project 3D/freeglut project 3D/MontanaRusa.cpp
@@ -9,3 +9,18 @@ MontanaRusa::MontanaRusa(void)
 MontanaRusa::~MontanaRusa(void)
 {
 }
+
+GLfloat MontanaRusa::functionX(GLfloat t)
+{
+	return 3*cos(t);
+}
+
+GLfloat MontanaRusa::functionY(GLfloat t)
+{
+	return 2*cos(t*1.5);
+}
+
+GLfloat MontanaRusa::functionZ(GLfloat t)
+{
+	return 3*sin(t);
+}
