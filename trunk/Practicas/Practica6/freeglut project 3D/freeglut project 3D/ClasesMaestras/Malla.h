09063ef37d97e42f8d0aea2f48e05e070753e4f2
cacu2ggg <cacu2ggg@2188cf36-fe29-5e81-ddad-f2da4cde5937>
2014-04-21 17:05:04 +0000

    Algunos añadidos

--- trunk/Practicas/Practica6/freeglut project 3D/freeglut project 3D/ClasesMaestras/Malla.h
+++ trunk/Practicas/Practica6/freeglut project 3D/freeglut project 3D/ClasesMaestras/Malla.h
@@ -5,8 +5,20 @@ class Malla : public Objeto3D
 {
 private:
 	//TODO color
+	//TODO vector con caras
+	//TODO vector con puntos
+	//TODO vector con normales/puntos ??
 public:
+	int nCaras, nPuntos;
 	Malla(void); 
 	~Malla(void);
+
+	virtual void calculaPuntos(void)=0;
+	virtual void calculaCaras(void)=0;
+
+	void dibuja(void){
+		// TODO recorrer vector de caras y pintar
+	}
+
 };
 
