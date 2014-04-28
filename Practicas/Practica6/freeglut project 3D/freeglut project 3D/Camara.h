0956206fc7ff85b4fd81e41968efc82823ffa5ca
raquelpm23 <raquelpm23@2188cf36-fe29-5e81-ddad-f2da4cde5937>
2014-04-28 11:44:35 +0000

    mas vistas - Sin probar

--- Practicas/Practica6/freeglut project 3D/freeglut project 3D/Camara.h
+++ Practicas/Practica6/freeglut project 3D/freeglut project 3D/Camara.h
@@ -15,8 +15,21 @@ public:
 	void roll(GLdouble alfa);
 	void yaw(GLdouble alfa);
 	void pitch(GLdouble alfa);
+	void desplazar(GLdouble x, GLdouble y, GLdouble z);
+	void ortogonal();
+	void perspectiva();
+	void oblicua();
+	void recorridoEje(GLdouble x, GLdouble y, GLdouble z);
+	void giraX(GLdouble alfa);
+	void giraY(GLdouble alfa);
+	void giraZ(GLdouble alfa);
+	void lateral();
+	void cenital();
+	void frontal();
+	void esquina();
 	
 	void getCoordCam();
 	void getMatriz();
+	void fijarCam();
 };
 
