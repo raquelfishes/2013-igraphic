f74d035930d69f8bfabc3615a4796dbeb80de2a7
raquelpm23 <raquelpm23@2188cf36-fe29-5e81-ddad-f2da4cde5937>
2014-04-29 22:00:03 +0000

    clase bola

---
+++ trunk/Practicas/Practica6/freeglut project 3D/freeglut project 3D/Elementos/Bola.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "../ClasesMaestras/ObjetoCompuesto.h"
+#include "../Figuras/Esfera.h"
+
+class Bola : public ObjetoCompuesto
+{
+public:
+	Bola(void);
+	Bola(GLdouble r, GLdouble g, GLdouble b);
+	~Bola(void);
+};
+
