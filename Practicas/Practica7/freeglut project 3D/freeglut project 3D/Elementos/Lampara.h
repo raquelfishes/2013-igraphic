9c55e8dcfb71927e68878fd4b9f45f22d4d46d82
cacu2ggg <cacu2ggg@2188cf36-fe29-5e81-ddad-f2da4cde5937>
2014-05-26 12:32:05 +0000

    la lampara

---
+++ Practicas/Practica7/freeglut project 3D/freeglut project 3D/Elementos/Lampara.h
@@ -0,0 +1,15 @@
+//Carlos Giraldo
+//Raquel Peces
+#pragma once
+#include "../ClasesMaestras/ObjetoCompuesto.h"
+#include "../Figuras/Cilindro.h"
+#include "../Figuras/Disco.h"
+#include "../Figuras/Cubo.h"
+#include "../Figuras/Esfera.h"
+
+class Lampara	: public ObjetoCompuesto{
+public:
+	Lampara(void);
+	~Lampara(void);
+};
+
