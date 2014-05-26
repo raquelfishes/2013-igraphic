a867d5ffdd54938db6dd5e6a1151b71ea4cd5577
cacu2ggg <cacu2ggg@2188cf36-fe29-5e81-ddad-f2da4cde5937>
2014-05-26 18:51:41 +0000

    Lampara que se escala a voluntad. La luz 1 parece que no hace nada, porque?????

--- trunk/Practicas/Practica7/freeglut project 3D/freeglut project 3D/Elementos/Lampara.cpp
+++ trunk/Practicas/Practica7/freeglut project 3D/freeglut project 3D/Elementos/Lampara.cpp
@@ -7,36 +7,42 @@ Lampara::Lampara(void){
 
 	// Base
 	Cubo *base = new Cubo(5);
-	base->obTraslate(0.0,0.0,2.5);
+	base->obRotate(90.0,0);
+	base->obTraslate(0.0,0.0,-42.5);
 	base->obScale(30.0,30.0,5.0);
 	base->setColor(1.0,1.0,1.0);
 	addComponente(base);
 
 	// Tubo
 	Cilindro *a=new Cilindro(1.5,1.5,20.0,15,10);
-	a->obTraslate(0.0,0.0,5.0);
+	a->obRotate(90.0,0);
+	a->obTraslate(0.0,0.0,-40.0);
 	a->setColor(1.0,1.0,1.0);
 	addComponente(a);
 
 	// Cono
 	Disco *d1=new Disco(1.5,7.5,15,2);
-	d1->obTraslate(0.0,0.0,25.0);
+	d1->obRotate(90.0,0);
+	d1->obTraslate(0.0,0.0,-20.0);
 	d1->obRotate(180.0,1);
 	d1->setColor(1.0,1.0,1.0);
 	addComponente(d1);
 
 	Cilindro *b=new Cilindro(7.5,7.5,5.0,15,3);
-	b->obTraslate(0.0,0.0,25.0);
+	b->obRotate(90.0,0);
+	b->obTraslate(0.0,0.0,-20.0);
 	b->setColor(1.0,1.0,1.0);
 	addComponente(b);
 
 	Disco *d2=new Disco(0.0,7.5,15,2);
-	d2->obTraslate(0.0,0.0,30.0);
+	d2->obRotate(90.0,0);
+	d2->obTraslate(0.0,0.0,-15.0);
 	d2->setColor(1.0,1.0,1.0);
 	addComponente(d2);
 
 	Cilindro *c1=new Cilindro(7.5,30.0,15.0,15,5);
-	c1->obTraslate(0.0,0.0,30.0);
+	c1->obRotate(90.0,0);
+	c1->obTraslate(0.0,0.0,-15.0);
 	c1->setColor(1.0,1.0,1.0);
 	addComponente(c1);
 
