d42d1a86ecac930330efa207ffc2e2f4c428c835
cacu2ggg <cacu2ggg@2188cf36-fe29-5e81-ddad-f2da4cde5937>
2014-05-27 16:16:30 +0000

    JOER MENUDA CHORRADA QUE ERA, HABÍA QUE METER LA MATRIZ A LA HORA DE PONER LA ******* LUZ

--- Practicas/Practica7/freeglut project 3D/freeglut project 3D/Elementos/Lampara.h
+++ Practicas/Practica7/freeglut project 3D/freeglut project 3D/Elementos/Lampara.h
@@ -15,13 +15,57 @@ using namespace std;
 
 #include <GL/freeglut.h>
 
-
 class Lampara	: public ObjetoCompuesto{
 
 public:
-
+	GLfloat focoA;
+	void increaseA(void){
+		focoA += 1.0;
+	}
+	void decreaseA(void){
+		focoA -= 1.0;
+	}
 	Lampara(void);
+	void dibuja(void){
+		
+		GLfloat focoP[] = {0.0, 0.0, 0.0, 1.0};
+		GLfloat dir[]={0.0,-1.0,0.0};
 
+		cout << "blablabla" << endl;
+		// LIGHT0
+		glMatrixMode(GL_MODELVIEW);
+		glPushMatrix();
+			glMultMatrixf(ta->getM());
+				// Foco de la lámpara
+			//glEnable(GL_LIGHT0);
+			glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, focoA);
+			glLightf(GL_LIGHT0, GL_SPOT_EXPONENT, 4.0);
+	
+
+			GLfloat d[]={1.0,1.0,1.0,1.0};
+			glLightfv(GL_LIGHT0, GL_DIFFUSE, d);
+			GLfloat a[]={0.3f,0.3f,0.3f,1.0};
+			glLightfv(GL_LIGHT0, GL_AMBIENT, a); 
+
+				
+			//GLfloat *auxV = ta->multV4(0.0, 150.0, 0.0, 1.0);
+			//GLfloat focoP[] = {auxV[0],auxV[1],auxV[2],auxV[3]};
+			//cout << focoP[0]<<" " <<focoP[1]<<" " <<focoP[2]<<" " <<focoP[3]<<" " << endl;
+			glLightfv(GL_LIGHT0, GL_POSITION, focoP);
+				
+
+			//auxV = ta->multV4(0.0,-1.0,0.0,0.0);
+			//Lfloat dir[] = {auxV[0],auxV[1],auxV[2]};
+			//cout << dir[0]<<" " <<dir[1]<<" " <<dir[2]<<" " <<endl;
+			glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, dir);
+
+			
+			glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, focoA);
+			
+		glPopMatrix();
+
+			ObjetoCompuesto::dibuja();
+	};
 	~Lampara(void);
 };
 
